8d9c51f89bdf7a2fd0ccaab0101d639ca5376efb
Per Lönn Wege <per@perlw.se>
2024-11-15 20:23:00 +0100

    convert textnode to leafnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -21,6 +21,37 @@ class TestTextNode(unittest.TestCase):
     def test_without_url(self):
         node = TextNode("This is a text node", TextType.NORMAL)
         self.assertEqual(node.url, None)
+
+    def test_to_html_node_normal(self):
+        node = TextNode("Text", TextType.NORMAL)
+        html = text_node_to_html_node(node).to_html()
+        self.assertEqual(html, "Text")
+
+    def test_to_html_node_bold(self):
+        node = TextNode("Text", TextType.BOLD)
+        html = text_node_to_html_node(node).to_html()
+        self.assertEqual(html, "<b>Text</b>")
+
+    def test_to_html_node_italic(self):
+        node = TextNode("Text", TextType.ITALIC)
+        html = text_node_to_html_node(node).to_html()
+        self.assertEqual(html, "<i>Text</i>")
+
+    def test_to_html_node_code(self):
+        node = TextNode("Text", TextType.CODE)
+        html = text_node_to_html_node(node).to_html()
+        self.assertEqual(html, "<code>Text</code>")
+
+    def test_to_html_node_link(self):
+        node = TextNode("Text", TextType.LINK, "https://boot.dev")
+        html = text_node_to_html_node(node).to_html()
+        self.assertEqual(html, "<a href=\"https://boot.dev\">Text</a>")
+
+    def test_to_html_node_image(self):
+        node = TextNode("Text", TextType.IMAGE, "https://boot.dev/favicon.ico")
+        html = text_node_to_html_node(node).to_html()
+        self.assertEqual(html, "<img src=\"https://boot.dev/favicon.ico\" alt=\"Text\"></img>")
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from typing import Self
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -22,3 +23,21 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    match(text_node.text_type):
+        case TextType.NORMAL:
+            return LeafNode(tag = None, value = text_node.text)
+        case TextType.BOLD:
+            return LeafNode(tag = "b", value = text_node.text)
+        case TextType.ITALIC:
+            return LeafNode(tag = "i", value = text_node.text)
+        case TextType.CODE:
+            return LeafNode(tag = "code", value = text_node.text)
+        case TextType.LINK:
+            return LeafNode(tag = "a", value = text_node.text, props = { "href": text_node.url })
+        case TextType.IMAGE:
+            return LeafNode(tag = "img", value = "", props = { "src": text_node.url, "alt": text_node.text })
+        case _:
+            raise Exception("unknown text type")
