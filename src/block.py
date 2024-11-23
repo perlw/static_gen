a86085301b0585dae0f9d97d41e57882c1d7d03a
Per Lönn Wege <per@perlw.se>
2024-11-23 12:46:40 +0100

    block types

--- src/block.py
+++ src/block.py
@@ -1,4 +1,13 @@
+from enum import Enum
 import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     result = []
@@ -9,3 +18,28 @@ def markdown_to_blocks(markdown: str) -> list[str]:
             continue
         result.append(stripped)
     return result
+
+def block_to_block_type(block: str) -> BlockType:
+    if re.match(r"#+ ", block) != None:
+        return BlockType.HEADING
+    elif block[:3] == "```" and block[-3:] == "```":
+        return BlockType.CODE
+
+    lines = block.splitlines()
+    is_quote = True
+    is_unordered = True
+    is_ordered = True
+    for line in lines:
+        if line[:2] != "> ":
+            is_quote = False
+        if line[:2] != "* " and line[:1] != "- ":
+            is_unordered = False
+        if not line[0].isdigit() and line[1] != '.':
+            is_ordered = False
+    if is_quote:
+        return BlockType.QUOTE
+    if is_unordered:
+        return BlockType.UNORDERED_LIST
+    if is_ordered:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_block.py
+++ src/test_block.py
@@ -1,25 +1,60 @@
 import unittest
 
-from block import markdown_to_blocks
+from block import BlockType, markdown_to_blocks, block_to_block_type
 
-
-class TestBlock(unittest.TestCase):
-    def test_eq(self):
-        result = markdown_to_blocks('''# This is a heading
+markdown = '''# This is a heading
 
 This is a paragraph of text. It has some **bold** and *italic* words inside of it.
 
 * This is the first list item in a list block
 * This is a list item
-* This is another list item''')
+* This is another list item
+
+```
+def foo="bar"
+```
+
+1. Foo
+1. Bar
+
+> Cinnamonrolls
+> are
+> amazing
+'''
+
+class TestBlock(unittest.TestCase):
+    def test_to_blocks(self):
+        result = markdown_to_blocks(markdown)
         expected = [
             "# This is a heading",
             "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
             '''* This is the first list item in a list block
 * This is a list item
 * This is another list item''',
+            '''```
+def foo="bar"
+```''',
+            '''1. Foo
+1. Bar''',
+            '''> Cinnamonrolls
+> are
+> amazing''',
         ]
         self.assertEqual(result, expected)
+
+    def test_block_type(self):
+        blocks = markdown_to_blocks(markdown)
+        expected = [
+            BlockType.HEADING,
+            BlockType.PARAGRAPH,
+            BlockType.UNORDERED_LIST,
+            BlockType.CODE,
+            BlockType.ORDERED_LIST,
+            BlockType.QUOTE,
+        ]
+        for i in range(0, len(expected)):
+            result = block_to_block_type(blocks[i])
+            self.assertEqual(result, expected[i])
 
 
 if __name__ == "__main__":
