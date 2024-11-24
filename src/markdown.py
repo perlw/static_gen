34fb8767363970a599282ffe0a6e98b34e597123
Per Lönn Wege <per@perlw.se>
2024-11-24 12:09:58 +0100

    janky markdown parser

---
+++ src/markdown.py
@@ -0,0 +1,59 @@
+from block import BlockType, markdown_to_blocks, block_to_block_type
+from htmlnode import ParentNode, LeafNode
+from textnode import text_to_textnodes, text_node_to_html_node
+
+def text_to_children(markdown: str) -> list[LeafNode]:
+    children = []
+    text_nodes = text_to_textnodes(markdown)
+    for t in text_nodes:
+        node = text_node_to_html_node(t)
+        children.append(node)
+    return children
+
+def markdown_to_html_node(markdown: str) -> ParentNode:
+    top_nodes = []
+
+    blocks = markdown_to_blocks(markdown)
+    for b in blocks:
+        block_type = block_to_block_type(b)
+
+        match block_type:
+            case BlockType.HEADING:
+                heading_depth = b.count('#')
+                if heading_depth > 6:
+                    heading_depth = 6
+
+                node = LeafNode(f"h{heading_depth}", b[heading_depth + 1:])
+                top_nodes.append(node)
+
+            case BlockType.PARAGRAPH:
+                children = text_to_children(b)
+                node = ParentNode("p", children)
+                top_nodes.append(node)
+
+            case BlockType.CODE:
+                node = ParentNode("pre", [
+                    LeafNode("code", b[4:-4])
+                ])
+                top_nodes.append(node)
+
+            case BlockType.QUOTE:
+                text = '\n'.join(map(lambda x: x[2:], b.splitlines()))
+                children = text_to_children(text)
+                node = ParentNode("blockquote", children)
+                top_nodes.append(node)
+
+            case BlockType.UNORDERED_LIST:
+                items = list(map(lambda x: x[2:], b.splitlines()))
+                children = list(map(lambda x: ParentNode("li", text_to_children(x)), items))
+                node = ParentNode("ul", children)
+                top_nodes.append(node)
+
+            case BlockType.ORDERED_LIST:
+                items = list(map(lambda x: x[3:], b.splitlines()))
+                children = list(map(lambda x: ParentNode("li", text_to_children(x)), items))
+                node = ParentNode("ol", children)
+                top_nodes.append(node)
+
+    root = ParentNode("div", top_nodes)
+    return root
---
+++ src/test_markdown.py
@@ -0,0 +1,64 @@
+import unittest
+
+from htmlnode import ParentNode, LeafNode
+from markdown import markdown_to_html_node
+
+markdown = '''# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+
+```
+def foo="bar"
+```
+
+1. Foo
+1. Bar
+
+> Cinnamonrolls
+> are
+> amazing
+'''
+
+class TestBlock(unittest.TestCase):
+    def test_to_blocks(self):
+        result = markdown_to_html_node(markdown)
+        expected = ParentNode("div", [
+            LeafNode("h1", "This is a heading"),
+            ParentNode("p", [
+                LeafNode(None, "This is a paragraph of text. It has some "),
+                LeafNode("b", "bold"),
+                LeafNode(None, " and "),
+                LeafNode("i", "italic"),
+                LeafNode(None, " words inside of it."),
+            ]),
+            ParentNode("ul", [
+                ParentNode("li", [
+                    LeafNode(None, "This is the first list item in a list block"),
+                ]),
+                ParentNode("li", [
+                    LeafNode(None, "This is a list item"),
+                ]),
+                ParentNode("li", [
+                    LeafNode(None, "This is another list item"),
+                ]),
+            ]),
+            ParentNode("pre", [
+                LeafNode("code", "def foo=\"bar\""),
+            ]),
+            ParentNode("ol", [
+                ParentNode("li", [
+                    LeafNode(None, "Foo"),
+                ]),
+                ParentNode("li", [
+                    LeafNode(None, "Bar"),
+                ]),
+            ]),
+            ParentNode("blockquote", [
+                LeafNode(None, "Cinnamonrolls\nare\namazing"),
+            ]),
+        ])
+        self.assertEqual(result, expected)
