24e1cf6afb319cc6b73a0127d34650fa98918a84
Per Lönn Wege <per@perlw.se>
2024-11-24 12:59:22 +0100

    full working static gen

--- src/main.py
+++ src/main.py
@@ -16,17 +16,33 @@ def copy_static_files(source: str, dest: str):
 def generate_page(from_path: str, template_path: str, dest_path: str):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
-    md = ""
+    markdown = ""
+    template = ""
     try:
-        file = open(from_path, )
-        md = file.read()
+        file = open(from_path)
+        markdown = file.read()
         file.close()
     except Exception as e:
         print(f"could not open {from_path}: {e}")
 
-    html_nodes = markdown_to_html_node(md)
-    print(html_nodes)
-    print(html_nodes.to_html())
+    try:
+        file = open(template_path)
+        template = file.read()
+        file.close()
+    except Exception as e:
+        print(f"could not open {template_path}: {e}")
+
+    title = extract_title(markdown)
+    content_html = markdown_to_html_node(markdown).to_html()
+    out_html = template.replace("{{ Title }}", title).replace("{{ Content }}", content_html)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok = True)
+    try:
+        file = open(dest_path, 'w')
+        file.write(out_html)
+        file.close()
+    except Exception as e:
+        print(f"could not open {dest_path}: {e}")
 
 def main():
     if os.path.exists("./public"):
