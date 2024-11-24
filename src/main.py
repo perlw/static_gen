a4e46f0d341a5174e81793bbb13be5a1e380ca8b
Per Lönn Wege <per@perlw.se>
2024-11-24 12:28:48 +0100

    copy static

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode
+import os
+import shutil
 
+def copy_static_files(dest, source):
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    for path in os.listdir(source):
+        fullpath = os.path.join(source, path)
+        if os.path.isdir(fullpath):
+            copy_static_files(os.path.join(dest, path), fullpath)
+        elif os.path.isfile(fullpath):
+            shutil.copy(fullpath, os.path.join(dest, path))
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
-    print(node)
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+    copy_static_files("./public", "./static")
 
 
 if __name__ == "__main__":
